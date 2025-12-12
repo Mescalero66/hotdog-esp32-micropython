115b4d7bf54f074f0a097404e1a937cbd0d6b885
powertec-jono <jonathon.mclauchlan@power-tec.com.au>
2025-12-12 17:21:50 +1100

    202512111654

--- mmmmmain.py
+++ mmmmmain.py
@@ -8,15 +8,20 @@ import hardware.WLAN_KEY as wlan_key
 ### CONSTANTS ###
 WIFI_SSID = wlan_key.ssid
 WIFI_PW = wlan_key.password
+
 BLUE_LED = 8
 SDA_PIN = 6
 SCL_PIN = 7
 ONEWIRE_PIN = 3
+LOAD_RELAY_PIN = 5
+
 UTC_OFFSET = 10 * 60 * 60
+SLEEP_MINUTES = 0.5
 
 ### OBJ SETUP ###
 wlan = network.WLAN(network.STA_IF)
-led = Pin(8, Pin.OUT)
+led = Pin(BLUE_LED, Pin.OUT)
+load_relay = Pin(LOAD_RELAY_PIN, Pin.OUT)
 real_time_clock = RTC()  
 ow_sensor_bus = OW_SENSOR.DS18X20(onewire.OneWire(Pin(ONEWIRE_PIN)))
 ow_sensor_array = ow_sensor_bus.scan()
@@ -24,18 +29,21 @@ i2c_sensor = I2C_SENSOR(ID=0, SCL=Pin(SCL_PIN), SDA=Pin(SDA_PIN))
 
 ### FUNCTIONS ###
 def blink_led():
-    led.high()
+    led.off()
     time.sleep_ms(100)
-    led.low()
+    led.on()
 
 def log(logline):
+    if "logs" not in os.listdir():
+        os.mkdir("logs")
     lt = time.localtime()
     logline = str(logline)
-    filename = "hotdog_{:04d}{:02d}{:02d}.txt".format(lt[0], lt[1], lt[2])
+    filename = "logs/hotdog_{:04d}{:02d}{:02d}.txt".format(lt[0], lt[1], lt[2])
     with open(filename, "a") as text_file:
         timestamp = "{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(lt[0], lt[1], lt[2], lt[3], lt[4], lt[5])
         text_file.write(f"{timestamp} - {logline} \n")
 
+load_relay.on()
 
 ### HOUSEKEEPING ###
 print(f"CPU Temp: {esp32.mcu_temperature()}°C")
@@ -56,23 +64,26 @@ print(f"CPU Temp: {esp32.mcu_temperature()}°C")
 #     real_time_clock.datetime(local_time)
 # print(time.localtime())
 
+
 i = 0
+j = 0
 ow_sensor_bus.convert_temp()
 time.sleep_ms(1000)
 while (i < 10):          
-    led.off()
-    for UID in ow_sensor_array:
-        temp_value = ow_sensor_bus.read_temp(UID)
-    Temp, Humi = i2c_sensor.get_CHT8305_TEMPERATURE_HUMIDITY()
-    print(f"Time: {time.localtime()} - OW: {temp_value:.1f}°C - I2C: {Temp:.1f}°C - Humi: {Humi:.1f}%")
-    log(f"OW: {temp_value:.1f}°C - I2C: {Temp:.1f}°C - Humi: {Humi:.1f}%")
-    led.on() 
-    if i == 5:
-        log(f"Good night! [CPU Temp: {esp32.mcu_temperature()}°C]")
-        # print(f"Good night: {time.localtime()} - CPU Temp: {esp32.mcu_temperature()}°C")
-        machine.lightsleep(5000)
-        log(f"Good morning! [CPU Temp: {esp32.mcu_temperature()}°C]")
-        # print(f"Good morning: {time.localtime()} - CPU Temp: {esp32.mcu_temperature()}°C")
-    else:
+    while (j < 4):
+        blink_led()
+        for UID in ow_sensor_array:
+            temp_value = ow_sensor_bus.read_temp(UID)
+        Temp, Humi = i2c_sensor.get_CHT8305_TEMPERATURE_HUMIDITY()
+        print(f"Time: {time.localtime()} - OW: {temp_value:.1f}°C - I2C: {Temp:.1f}°C - Humi: {Humi:.1f}%")
+        log(f"OW: {temp_value:.1f}°C - I2C: {Temp:.1f}°C - Humi: {Humi:.1f}%")
+        ow_sensor_bus.convert_temp()
         time.sleep(1)
+        j += 1
+    j = 0
+    load_relay.off()
+    log(f"SLEEP [CPU: {esp32.mcu_temperature()}°C]")
+    machine.lightsleep(int(SLEEP_MINUTES * 1000 * 60))
+    log(f"WAKE! [CPU: {esp32.mcu_temperature()}°C]")
+    load_relay.on()
     i += 1
